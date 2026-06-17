3a68c718214d135b56176400905a411769e2f585
ibrahemabbakar-debug <ibrahemabbakar@gmail.com>
2026-06-17 21:49:51 +0200

    Add RootPage component for user redirection

---
+++ src/app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation'
+import { createServerSupabaseClient } from '@/lib/supabase/server'
+
+export default async function RootPage() {
+  const supabase = await createServerSupabaseClient()
+  const { data: { user } } = await supabase.auth.getUser()
+  if (user) redirect('/buyer/home')
+  redirect('/auth/login')
+}
